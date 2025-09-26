9f66f11f51f8076bc47c0ea1d165b37c1a7cea84
enigmat <36648706+enigmat@users.noreply.github.com>
2025-09-26 16:21:39 -0400

    feat: Add 'Club' genre to music constants
    Adds the 'Club' genre to the list of available music genres in the constants file. This provides users with an additional option when generating songs.

--- constants/music.ts
+++ constants/music.ts
@@ -1,5 +1,5 @@
 export const genres = [
-    'Pop', 'Rock', 'Hip Hop / Rap', 'R&B / Soul', 'Electronic', 'Country', 'Jazz', 'Classical', 'Latin', 'Metal', 'Indie', 'Alternative', 'Folk', 'Acoustic', 'Afrobeat', 'Ambient', 'Blues', 'Dance', 'Dancehall', 'Disco', 'Drum & Bass', 'Dubstep', 'Funk', 'Gospel', 'House', 'Instrumental', 'Lo-fi', 'Punk', 'Reggae', 'Synthwave', 'Techno', 'Trance', 'Trap'
+    'Pop', 'Rock', 'Hip Hop / Rap', 'R&B / Soul', 'Electronic', 'Country', 'Jazz', 'Classical', 'Latin', 'Metal', 'Indie', 'Alternative', 'Folk', 'Acoustic', 'Afrobeat', 'Ambient', 'Blues', 'Club', 'Dance', 'Dancehall', 'Disco', 'Drum & Bass', 'Dubstep', 'Funk', 'Gospel', 'House', 'Instrumental', 'Lo-fi', 'Punk', 'Reggae', 'Synthwave', 'Techno', 'Trance', 'Trap'
 ];
 
 export const vibes = [
